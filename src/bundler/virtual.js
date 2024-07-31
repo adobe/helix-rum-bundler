5feac83d7d6475cb18578c59655905355f467a7f
Max Edell <maxed@adobe.com>
2024-07-31 09:14:08 -0700

    fix: handle invalid hosts

--- src/bundler/virtual.js
+++ src/bundler/virtual.js
@@ -63,7 +63,7 @@ export default [{
         ...e,
         weight: e.weight * 100,
         domain: info.domain,
-        hostType: e.host.endsWith('.adobeaemcloud.net') ? 'aemcs' : 'helix',
+        hostType: (typeof e.host === 'string' && e.host.endsWith('.adobeaemcloud.net')) ? 'aemcs' : 'helix',
       },
     };
   },
