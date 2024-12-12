15e64fd79b2ea841efb1aa4d9f54a5cc6e380886
Max Edell <maxed@adobe.com>
2024-12-12 06:24:45 -0800

    fix: add new sidekick virtual (#260)

--- src/bundler/virtual.js
+++ src/bundler/virtual.js
@@ -29,6 +29,22 @@ export default [{
     };
   },
 }, {
+  domain: 'aem.live:sidekick',
+  test: (e) => e.source === 'sidekick',
+  destination(e, info) {
+    return {
+      key: `/${this.domain}/${info.year}/${info.month}/${info.day}/${info.hour}.json`,
+      info: {
+        ...info,
+        domain: this.domain,
+      },
+      event: {
+        ...e,
+        domain: info.domain,
+      },
+    };
+  },
+}, {
   // all top events, for viewing all domains' events
   // downsample by 100x
   domain: 'all',
